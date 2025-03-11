638680300343e38a7379015806ecfce9cf91ef92
DigitalDreamNL <github@digitaldream.nl>
2025-03-11 13:59:18 +0100

    Puzzle 5.

---
+++ InternationalizationPuzzles2025.Test/Puzzles/Puzzle5/Puzzle5Tests.cs
@@ -0,0 +1,46 @@
+﻿using Shouldly;
+
+namespace InternationalizationPuzzles2025.Test.Puzzles.Puzzle5;
+
+public class Puzzle5Tests
+{
+    private readonly string[] _testInput =
+    [
+        " ⚘   ⚘ ",
+        "  ⸫   ⸫",
+        "🌲   💩  ",
+        "     ⸫⸫",
+        " 🐇    💩",
+        "⸫    ⸫ ",
+        "⚘🌲 ⸫  🌲",
+        "⸫    🐕 ",
+        "  ⚘  ⸫ ",
+        "⚘⸫⸫   ⸫",
+        "  ⚘⸫   ",
+        " 💩  ⸫  ",
+        "     ⸫⸫"
+    ];
+
+    [Fact]
+    public void Test()
+    {
+        var puzzle = CreatePuzzleWithDefaultTestInput();
+        puzzle.Execute().ShouldBe("2");
+    }
+
+    [Fact]
+    public void CalculateNextPosition()
+    {
+        var puzzle = CreatePuzzleWithDefaultTestInput();
+        puzzle.CalculateNextXPosition(0).ShouldBe(2);
+        puzzle.CalculateNextXPosition(3).ShouldBe(5);
+        puzzle.CalculateNextXPosition(6).ShouldBe(1);
+    }
+
+    private InternationalizationPuzzles2025.Puzzles.Puzzle5.Puzzle5 CreatePuzzleWithDefaultTestInput()
+    {
+        var puzzle = new InternationalizationPuzzles2025.Puzzles.Puzzle5.Puzzle5();
+        puzzle.SetInput(_testInput);
+        return puzzle;
+    }
+}
---
+++ InternationalizationPuzzles2025/Puzzles/Puzzle5/Puzzle5.cs
@@ -0,0 +1,30 @@
+﻿namespace InternationalizationPuzzles2025.Puzzles.Puzzle5;
+
+public class Puzzle5 : Puzzle
+{
+    protected override string FileName => "puzzle5.txt";
+
+    private int Width => _width ??= Input[0].Length;
+    private int? _width;
+
+    private int _xPosition;
+    private int _numberOfPoos;
+
+    public override string Execute()
+    {
+        foreach (var line in Input)
+            HandleLine(line);
+
+        return _numberOfPoos.ToString();
+    }
+
+    private void HandleLine(string line)
+    {
+        if (line[_xPosition] == '\ud83d') // 💩
+            _numberOfPoos++;
+
+        _xPosition = CalculateNextXPosition(_xPosition);
+    }
+
+    public int CalculateNextXPosition(int x) => (x + 2) % Width;
+}
